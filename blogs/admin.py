65eb792a8b4c24879aab99bfe1fb2bfbe17c42c3
Nicholas <970649831@qq.com>
2018-04-25 17:55:31 +0800

    配置admin

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from blogs.models import Blog
 # Register your models here.
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'description', 'create_date')
+    list_filter = ('title', )
+    ordering = ('id', )
+
+admin.site.register(Blog, BlogAdmin)
+
+
