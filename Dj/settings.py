025ed9714394a59a9093198cf850dd23b091c8a5
泽娄 <970649831@qq.com>
2018-05-08 22:25:20 +0800

    添加评论

--- Dj/settings.py
+++ Dj/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blogs',
+    'comments',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
