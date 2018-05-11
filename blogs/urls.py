00b93c8332093be9e6bed41884461056b6312e4b
Nicholas <970649831@qq.com>
2018-05-11 14:49:45 +0800

    收集静态文件

---
+++ blogs/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url, include
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+#router.register(r'news', NewsModelViewSet)
+
+blogs_urlpatterns = [
+    url(r'^', include(router.urls)),
+]
+
