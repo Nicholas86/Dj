235fcb3c0b150fc5792cebf63bbc206752c313a9
泽娄 <970649831@qq.com>
2018-05-17 20:50:34 +0800

    优化mysql容器启动

--- develop/reset.sh
+++ develop/reset.sh
@@ -15,7 +15,9 @@ ensure "removing existing services" docker-compose rm -fv
 ensure "removing existing services" docker-compose rm -fv
 
 #4.1清空elasticsearch索引、文档
-ensure "removing existing elasticsearch index-doc" curl -XDELETE http://192.168.40.10:9200/_all
+ensure "starting elasticsearch" brew services restart elasticsearch
+sleep 10
+ensure "removing existing elasticsearch index-doc" curl -XDELETE http://192.168.99.1:9200/_all
 
 #5.判断是否包含BUILD参数, BUILD=Y ./develop/reset.sh 参数名BUILD, 值Y
 if [ -n "${BUILD}" ]
